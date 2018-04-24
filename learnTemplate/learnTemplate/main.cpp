6c0f0bba1d1a1a078c2cce988e5f7e1a392b6f56
why2cs <why2cs@outlook.com>
2018-04-24 17:24:30 +0800

    dfgdf

--- learnTemplate/learnTemplate/main.cpp
+++ learnTemplate/learnTemplate/main.cpp
@@ -1,5 +1,24 @@
+template <typename T>
+int compare(const T &v1, const T &v2) {
+	if (v1 < v2) return -1;
+	if (v2 < v1) return 1;
+	return 0;
+}
+
+
+
+
+template <unsigned N,unsigned M>
+int compare(const char(&v1)[N], const char(&v2)[M]) {
+	return strcmp(v1, v2);
+}
+
+
+#include <iostream>
 
 int main()
 {
+	auto result = compare(13, 5);
+	auto result2 = compare("hi", "hello");
 	return 0;
 }
